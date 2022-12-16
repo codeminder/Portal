e9e47d3f17266c2ce69900990ba007ceb592f2b6
Roman <roman@example.com>
2022-12-16 07:49:20 +0200

    add cashagenda app

--- portal/portal/urls.py
+++ portal/portal/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mainpage.urls'))
+    path('', include('mainpage.urls')),
+    path('cashagenda/', include('cashagenda.urls'))
 ]
